b30f746495149fec620cd57a6adb6cd61cd7b5f7
Evgeny Pavlov <evgenypavlov666@yandex.ru>
2025-06-03 17:48:27 +0300

    refactor(AuthTokenServiceImpl): улучшена реализация обработки токенов

--- src/main/java/com/example/auth/service/jwt/AuthTokenServiceImpl.java
+++ src/main/java/com/example/auth/service/jwt/AuthTokenServiceImpl.java
@@ -3,25 +3,35 @@ package com.example.auth.service.jwt;
 import com.example.auth.dto.AuthTokens;
 import com.example.auth.entity.Role;
 import com.example.auth.entity.User;
+import com.example.auth.exception.token.ExpiredTokenException;
+import com.example.auth.exception.token.TokenBlacklistedException;
+import com.example.auth.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class AuthTokenServiceImpl implements AuthTokenService {
     private final JwtService jwtService;
+    private final UserService userService;
+    private final TokenBlacklistService tokenBlacklistService;
+
+
 
 
     @Override
     public AuthTokens generateAuthTokens(User user) {
         List<String> role = extractRoles(user);
+
         return AuthTokens.builder()
                 .accessToken(jwtService.generateAccessToken(
-                        user.getEmail(),
+                        user.getId().toString(),
                         role,
-                        user.getId().toString()
+                        user.getEmail()
                 ))
                 .refreshToken(jwtService.generateRefreshToken(user.getId().toString()))
                 .build();
@@ -31,5 +41,26 @@ public class AuthTokenServiceImpl implements AuthTokenService {
         return user.getRoles().stream()
                 .map(Role::getName)
                 .toList();
+    }
+
+    @Override
+    public AuthTokens refreshToken(String refreshToken) {
+        // 1. Проверяем, не отозван ли уже токен
+        if (tokenBlacklistService.isTokenBlacklisted(refreshToken)) {
+            throw new TokenBlacklistedException("Токен отозван");
+        }
+        // 2. Проверяем валидность токена
+        String userId = jwtService.extractSubject(refreshToken);
+        if (jwtService.isTokenExpired(refreshToken)) {
+            throw new ExpiredTokenException("Токен истёк");
+        }
+        // 3. Получаем пользователя
+        User user = userService.findById(UUID.fromString(userId));
+        tokenBlacklistService.removeFromBlacklist(refreshToken);
+        // 4. Добавляем старый токен в черный список
+        tokenBlacklistService.addToBlacklist(refreshToken, Duration.ofDays(30));
+
+        // 5. Генерируем новые токены
+        return generateAuthTokens(user);
     }
 }
