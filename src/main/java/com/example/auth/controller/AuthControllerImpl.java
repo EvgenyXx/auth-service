a86ced5b1183a22f4c714f880758384e9be52f6d
Evgeny Pavlov <evgenypavlov666@yandex.ru>
2025-06-03 17:39:20 +0300

    refactor(controller): Разделите AuthController на интерфейс и реализацию

--- src/main/java/com/example/auth/controller/AuthControllerImpl.java
+++ src/main/java/com/example/auth/controller/AuthControllerImpl.java
@@ -1,8 +1,10 @@
 package com.example.auth.controller;
 
 import com.example.auth.config.JwtCookieProperties;
+import com.example.auth.dto.AuthTokens;
 import com.example.auth.dto.UserRegisterRequest;
 import com.example.auth.dto.UserRegisterResponse;
+import com.example.auth.service.jwt.AuthTokenService;
 import com.example.auth.service.register.UserRegistrationService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -11,11 +13,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RequiredArgsConstructor
@@ -24,15 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthControllerImpl implements AuthController {
 
     private final UserRegistrationService userRegistrationService;
+    private final JwtCookieProperties cookieProperties;
+    private final AuthTokenService authTokenService;
 
     @Override
     @PostMapping("/register")
     public ResponseEntity<UserRegisterResponse> registerUser(
             @Valid @RequestBody UserRegisterRequest registerRequest,
-            HttpServletResponse response,
-            JwtCookieProperties cookieProperties) {
+            HttpServletResponse response) {
         UserRegisterResponse registerResponse = userRegistrationService.registerUser(registerRequest);
-        setRefreshTokenCookie(response, registerResponse,cookieProperties);
+        setRefreshTokenCookie(response, registerResponse);
         UserRegisterResponse userRegisterResponse = registerResponse.withoutTokens();
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -42,8 +41,7 @@ public class AuthControllerImpl implements AuthController {
     }
 
     private void setRefreshTokenCookie(HttpServletResponse response,
-                                       UserRegisterResponse registerResponse,
-                                       JwtCookieProperties cookieProperties) {
+                                       UserRegisterResponse registerResponse) {
         ResponseCookie responseCookie = ResponseCookie.from(cookieProperties.getName(),
                         registerResponse.getRefreshToken())
                 .httpOnly(cookieProperties.isHttpOnly())
@@ -54,4 +52,27 @@ public class AuthControllerImpl implements AuthController {
                 .build();
         response.addHeader(HttpHeaders.SET_COOKIE, responseCookie.toString());
     }
+
+    @Override
+    @PostMapping("/refresh")
+    public ResponseEntity<Void> refreshTokens(
+            @CookieValue("${spring.security.jwt.cookie.name}") String refreshToken,
+            HttpServletResponse response) {
+        AuthTokens authTokens = authTokenService.refreshToken(refreshToken);
+        ResponseCookie cookie = ResponseCookie.from(cookieProperties.getName(), authTokens.getRefreshToken())
+                .secure(cookieProperties.isSecure())
+                .sameSite(cookieProperties.getSameSite())
+                .path(cookieProperties.getPath())
+                .maxAge(cookieProperties.getMaxAge())
+                .httpOnly(cookieProperties.isHttpOnly())
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + authTokens.getAccessToken())
+                .build();
+    }
+
+
 }
