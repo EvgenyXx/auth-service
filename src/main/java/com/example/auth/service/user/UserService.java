1f22f94ffce179757a7e310a8a37c6e95fb5a466
Evgeny Pavlov <evgenypavlov666@yandex.ru>
2025-06-03 17:48:27 +0300

    refactor(UserService): обновлена структура сервисов для пользователя

--- src/main/java/com/example/auth/service/user/UserService.java
+++ src/main/java/com/example/auth/service/user/UserService.java
@@ -1,4 +1,25 @@
 package com.example.auth.service.user;
 
+import com.example.auth.dto.UserRegisterRequest;
+import com.example.auth.entity.User;
+
+import java.util.UUID;
+
 public interface UserService {
+
+    /*
+    Методы для UserValidationService.java
+     */
+    boolean existsByEmail(String email);
+
+    boolean existsByNumberPhone(String numberPhone);
+
+    User saveUser(User user);
+
+
+    User findById(UUID userId);
+
+    User createUser(User user);
+
+
 }
