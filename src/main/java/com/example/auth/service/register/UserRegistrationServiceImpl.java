b049446a99d4297a35fefbb9e49f06bb229666ff
Evgeny Pavlov <evgenypavlov666@yandex.ru>
2025-06-03 17:48:27 +0300

    refactor(UserRegistrationServiceImpl): улучшена логика регистрации пользователей

--- src/main/java/com/example/auth/service/register/UserRegistrationServiceImpl.java
+++ src/main/java/com/example/auth/service/register/UserRegistrationServiceImpl.java
@@ -5,17 +5,14 @@ import com.example.auth.dto.UserRegisterRequest;
 import com.example.auth.dto.UserRegisterResponse;
 import com.example.auth.dto.mapper.UserMapper;
 import com.example.auth.entity.User;
-import com.example.auth.repository.UserRepository;
 import com.example.auth.service.jwt.AuthTokenService;
-import com.example.auth.service.kafka.KafkaEventSender;
-import com.example.auth.service.role.RoleService;
+import com.example.auth.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+
 
 
 @Service
@@ -23,30 +20,23 @@ import java.util.List;
 @Log4j2
 public class UserRegistrationServiceImpl implements UserRegistrationService {
 
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
+    private final UserService userService;
     private final UserValidationService userValidationService;
-    private final PasswordEncoder passwordEncoder;
-    private final RoleService roleService;
-    private  final KafkaEventSender kafkaEventSender;
     private final AuthTokenService authTokenService;
+    private final UserMapper userMapper;
 
 
     @Override
     @Transactional
     public UserRegisterResponse registerUser(UserRegisterRequest userRegisterRequest) {
-        userValidationService.checkEmailUniqueness(userRegisterRequest.getEmail());
-        userValidationService.checkPhoneNumberUniqueness(userRegisterRequest.getNumberPhone());
-        User user = userMapper.toRegisterEntity(userRegisterRequest);
-        user.setPassword(passwordEncoder.encode(userRegisterRequest.getPassword()));
-        user.setRoles(List.of(roleService.getDefaultUserRole()));
-        User saveUser = userRepository.saveAndFlush(user);
-        kafkaEventSender.sendUserRegistrationEvent(user);
-        AuthTokens authTokens = authTokenService.generateAuthTokens(saveUser);
-        return userMapper.toRegisterResponse(saveUser,authTokens);
+        userValidationService.validateUserData(
+                userRegisterRequest.getEmail(),
+                userRegisterRequest.getNumberPhone()
+        );
+        User user = userService.createUser(userMapper.toRegisterEntity(userRegisterRequest));
+        AuthTokens authTokens = authTokenService.generateAuthTokens(user);
+        return userMapper.toRegisterResponse(user, authTokens);
     }
-
-
 
 
 }
