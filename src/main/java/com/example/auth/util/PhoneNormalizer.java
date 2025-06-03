bb940575f244a6cbd14aaa39726e0ead90083991
Evgeny Pavlov <evgenypavlov666@yandex.ru>
2025-06-03 17:48:27 +0300

    refactor(PhoneNormalizer): улучшена нормализация номера телефона

--- src/main/java/com/example/auth/util/PhoneNormalizer.java
+++ src/main/java/com/example/auth/util/PhoneNormalizer.java
@@ -1,4 +1,18 @@
 package com.example.auth.util;
 
+
+
+
 public class PhoneNormalizer {
+
+    private PhoneNormalizer() {
+    }
+
+    public static String normalize (String numberPhone){
+      String normalNumber = numberPhone.replaceAll("\\D","");
+      if (normalNumber.matches("^8\\d{10}$")){
+          return "7" + normalNumber.substring(1);
+      }
+      return normalNumber;
+    }
 }
