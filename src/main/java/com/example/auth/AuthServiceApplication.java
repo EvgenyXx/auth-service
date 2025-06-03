6d59a0f129b6088eabaf30311389f239e115bd71
Evgeny Pavlov <evgenypavlov666@yandex.ru>
2025-06-03 17:48:27 +0300

    refactor(AuthServiceApplication): улучшена структура приложения

--- src/main/java/com/example/auth/AuthServiceApplication.java
+++ src/main/java/com/example/auth/AuthServiceApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+
 @SpringBootApplication
 @EnableConfigurationProperties({JwtCookieProperties.class})
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
+
 
 	}
 
