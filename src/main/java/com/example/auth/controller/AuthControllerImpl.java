5204c90ffb4373a375237df1579c0548e9426252
evgeny <evgenypavlov666@yandex.ru>
2025-06-04 00:31:38 +0300

    добавил куки сервис

--- src/main/java/com/example/auth/controller/AuthControllerImpl.java
+++ src/main/java/com/example/auth/controller/AuthControllerImpl.java
@@ -1,17 +1,17 @@
 package com.example.auth.controller;
 
-import com.example.auth.config.JwtCookieProperties;
+
 import com.example.auth.dto.AuthTokens;
 import com.example.auth.dto.UserRegisterRequest;
 import com.example.auth.dto.UserRegisterResponse;
 import com.example.auth.service.jwt.AuthTokenService;
+import com.example.auth.service.jwt.CookieService;
 import com.example.auth.service.register.UserRegistrationService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,8 +21,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/auth")
 public class AuthControllerImpl implements AuthController {
 
+    private static final String AUTH_HEADER_PREFIX = "Bearer ";
+
     private final UserRegistrationService userRegistrationService;
-    private final JwtCookieProperties cookieProperties;
+    private final CookieService cookieService;
     private final AuthTokenService authTokenService;
 
     @Override
@@ -31,27 +33,16 @@ public class AuthControllerImpl implements AuthController {
             @Valid @RequestBody UserRegisterRequest registerRequest,
             HttpServletResponse response) {
         UserRegisterResponse registerResponse = userRegistrationService.registerUser(registerRequest);
-        setRefreshTokenCookie(response, registerResponse);
+        cookieService.setRefreshTokenCookie(response, registerResponse.getRefreshToken());
         UserRegisterResponse userRegisterResponse = registerResponse.withoutTokens();
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + registerResponse.getAccessToken())
+                .header(HttpHeaders.AUTHORIZATION, AUTH_HEADER_PREFIX
+                        + registerResponse.getAccessToken())
                 .body(userRegisterResponse);
 
     }
 
-    private void setRefreshTokenCookie(HttpServletResponse response,
-                                       UserRegisterResponse registerResponse) {
-        ResponseCookie responseCookie = ResponseCookie.from(cookieProperties.getName(),
-                        registerResponse.getRefreshToken())
-                .httpOnly(cookieProperties.isHttpOnly())
-                .maxAge(cookieProperties.getMaxAge())
-                .path(cookieProperties.getPath())
-                .secure(cookieProperties.isSecure())
-                .sameSite(cookieProperties.getSameSite())
-                .build();
-        response.addHeader(HttpHeaders.SET_COOKIE, responseCookie.toString());
-    }
 
     @Override
     @PostMapping("/refresh")
@@ -59,18 +50,11 @@ public class AuthControllerImpl implements AuthController {
             @CookieValue("${spring.security.jwt.cookie.name}") String refreshToken,
             HttpServletResponse response) {
         AuthTokens authTokens = authTokenService.refreshToken(refreshToken);
-        ResponseCookie cookie = ResponseCookie.from(cookieProperties.getName(), authTokens.getRefreshToken())
-                .secure(cookieProperties.isSecure())
-                .sameSite(cookieProperties.getSameSite())
-                .path(cookieProperties.getPath())
-                .maxAge(cookieProperties.getMaxAge())
-                .httpOnly(cookieProperties.isHttpOnly())
-                .build();
-        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
-
+        cookieService.setRefreshTokenCookie(response, authTokens.getRefreshToken());
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + authTokens.getAccessToken())
+                .header(HttpHeaders.AUTHORIZATION, AUTH_HEADER_PREFIX
+                        + authTokens.getAccessToken())
                 .build();
     }
 
